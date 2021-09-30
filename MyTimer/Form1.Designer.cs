869211c6fbbf17923ce46f0bb518f91195167b34
yuanky <yuanky123@qq.com>
2021-09-30 11:15:25 +0800

    init

---
+++ MyTimer/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace MyTimer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTimersTimer = new System.Windows.Forms.Button();
+            this.lblMainId = new System.Windows.Forms.Label();
+            this.btnThreadingTimer = new System.Windows.Forms.Button();
+            this.btnFormsTimer = new System.Windows.Forms.Button();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.lblSonId = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnTimersTimer
+            // 
+            this.btnTimersTimer.Location = new System.Drawing.Point(49, 43);
+            this.btnTimersTimer.Name = "btnTimersTimer";
+            this.btnTimersTimer.Size = new System.Drawing.Size(93, 23);
+            this.btnTimersTimer.TabIndex = 0;
+            this.btnTimersTimer.Text = "TimersTimer";
+            this.btnTimersTimer.UseVisualStyleBackColor = true;
+            this.btnTimersTimer.Click += new System.EventHandler(this.btnTimersTimer_Click);
+            // 
+            // lblMainId
+            // 
+            this.lblMainId.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblMainId.Location = new System.Drawing.Point(49, 94);
+            this.lblMainId.Name = "lblMainId";
+            this.lblMainId.Size = new System.Drawing.Size(74, 23);
+            this.lblMainId.TabIndex = 1;
+            this.lblMainId.Text = "label1";
+            this.lblMainId.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // btnThreadingTimer
+            // 
+            this.btnThreadingTimer.Location = new System.Drawing.Point(174, 43);
+            this.btnThreadingTimer.Name = "btnThreadingTimer";
+            this.btnThreadingTimer.Size = new System.Drawing.Size(112, 23);
+            this.btnThreadingTimer.TabIndex = 2;
+            this.btnThreadingTimer.Text = "ThreadingTimer";
+            this.btnThreadingTimer.UseVisualStyleBackColor = true;
+            this.btnThreadingTimer.Click += new System.EventHandler(this.btnThreadingTimer_Click);
+            // 
+            // btnFormsTimer
+            // 
+            this.btnFormsTimer.Location = new System.Drawing.Point(318, 43);
+            this.btnFormsTimer.Name = "btnFormsTimer";
+            this.btnFormsTimer.Size = new System.Drawing.Size(80, 23);
+            this.btnFormsTimer.TabIndex = 3;
+            this.btnFormsTimer.Text = "FormsTimer";
+            this.btnFormsTimer.UseVisualStyleBackColor = true;
+            this.btnFormsTimer.Click += new System.EventHandler(this.btnFormsTimer_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblMessage.Location = new System.Drawing.Point(49, 137);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(350, 23);
+            this.lblMessage.TabIndex = 4;
+            this.lblMessage.Text = "label2";
+            this.lblMessage.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblSonId
+            // 
+            this.lblSonId.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSonId.Location = new System.Drawing.Point(129, 94);
+            this.lblSonId.Name = "lblSonId";
+            this.lblSonId.Size = new System.Drawing.Size(74, 23);
+            this.lblSonId.TabIndex = 6;
+            this.lblSonId.Text = "label1";
+            this.lblSonId.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(319, 94);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Stop";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblCount
+            // 
+            this.lblCount.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCount.Location = new System.Drawing.Point(209, 94);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(74, 23);
+            this.lblCount.TabIndex = 8;
+            this.lblCount.Text = "label1";
+            this.lblCount.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 190);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblSonId);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.btnFormsTimer);
+            this.Controls.Add(this.btnThreadingTimer);
+            this.Controls.Add(this.lblMainId);
+            this.Controls.Add(this.btnTimersTimer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTimersTimer;
+        private System.Windows.Forms.Label lblMainId;
+        private System.Windows.Forms.Button btnThreadingTimer;
+        private System.Windows.Forms.Button btnFormsTimer;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Label lblSonId;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblCount;
+    }
+}
+
---
+++ MyTimer/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace MyTimer
+{
+    public partial class Form1 : Form
+    {
+        int interval = 1000;
+        int sleep = 0;
+        int count = 0;
+
+        //创建新线程,间隔interval后启动
+        System.Timers.Timer timersTimer;
+        //可指定延迟启动新线程,延迟为0时立即启动
+        System.Threading.Timer threadingTimer;
+        //由主线程执行,间隔interval后启动
+        System.Windows.Forms.Timer formsTimer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+            StopTimers();
+        }
+
+        private void btnTimersTimer_Click(object sender, EventArgs e)
+        {
+            lblMainId.Text = Thread.CurrentThread.ManagedThreadId.ToString();
+
+            timersTimer = new System.Timers.Timer();
+            //timersTimer.AutoReset = false;
+            timersTimer.Interval = this.interval;
+            timersTimer.Elapsed += (x, y) =>
+            {
+                var dt = DateTime.Now;
+                Thread.Sleep(sleep);
+                //lblSonId.Text = Thread.CurrentThread.ManagedThreadId.ToString();
+                //lblMessage.Text = DateTime.Now.ToString();
+                ShowMessageThread(Thread.CurrentThread.ManagedThreadId, dt);
+            };
+            timersTimer.Start();
+        }
+
+        private void btnThreadingTimer_Click(object sender, EventArgs e)
+        {
+            lblMainId.Text = Thread.CurrentThread.ManagedThreadId.ToString();
+
+            //跨线程访问
+            threadingTimer = new System.Threading.Timer((x) =>
+            {
+                var dt = DateTime.Now;
+                Thread.Sleep(sleep);
+                ShowMessageThread(Thread.CurrentThread.ManagedThreadId, dt);
+            }, null, 0, this.interval);
+
+        }
+
+        private void btnFormsTimer_Click(object sender, EventArgs e)
+        {
+            lblMainId.Text = Thread.CurrentThread.ManagedThreadId.ToString();
+
+            formsTimer = new System.Windows.Forms.Timer();
+            formsTimer.Interval = this.interval;
+            formsTimer.Tick += (x, y) =>
+            {
+                var dt = DateTime.Now;
+                Thread.Sleep(sleep);
+                ShowMessage(Thread.CurrentThread.ManagedThreadId, dt);
+            };
+            formsTimer.Start();
+        }
+
+
+        private void ShowMessageThread(int threadId, DateTime dt)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new Action<int, DateTime>(this.ShowMessage), threadId, dt);
+            }
+            else
+            {
+                ShowMessage(threadId, dt);
+            }
+        }
+
+        private void ShowMessage(int threadId, DateTime dt)
+        {
+            //lock (locker)
+            count++;
+
+
+            lblSonId.Text = threadId.ToString();
+            lblCount.Text = count.ToString();
+            lblMessage.Text = dt.ToString("hh:mm:ss.fff");
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+
+            StopTimers();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StopTimers();
+        }
+
+        private void StopTimers()
+        {
+            count = 0;
+            lblMainId.Text = lblSonId.Text = lblCount.Text = lblMessage.Text = "";
+
+            if (timersTimer != null) timersTimer.Dispose();
+            if (threadingTimer != null) threadingTimer.Dispose();
+            if (formsTimer != null) formsTimer.Dispose();
+        }
+    }
+}
